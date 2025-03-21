6cc70dee5e0e0227ac02a56177b06262113f8649
thly112 <thaolythly@gmail.com>
2025-03-21 08:37:37 +0700

    trang intro

--- app/src/main/java/com/example/project_gk/IntroActivity.java
+++ app/src/main/java/com/example/project_gk/IntroActivity.java
@@ -8,7 +8,36 @@ import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class IntroActivity extends AppCompatActivity {
+    private ConstraintLayout btnStart;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_intro);
+
+        btnStart = findViewById(R.id.btn_start);
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // Kiểm tra xem người dùng đã đăng nhập chưa
+                SharedPreferences sharedPreferences = getSharedPreferences("UserPrefs", MODE_PRIVATE);
+                boolean isLoggedIn = sharedPreferences.getBoolean("isLoggedIn", false);
+
+                if (isLoggedIn) {
+                    // Nếu đã đăng nhập, chuyển đến MainActivity
+                    startActivity(new Intent(IntroActivity.this, MainActivity.class));
+                } else {
+                    // Nếu chưa đăng nhập, chuyển đến LoginActivity
+                    startActivity(new Intent(IntroActivity.this, LoginActivity.class));
+                }
+                finish();
+            }
+        });
     }
+}
