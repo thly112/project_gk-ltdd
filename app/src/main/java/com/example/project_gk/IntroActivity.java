29ef761a1a59a4adeb223d6c3a9cb2fc58a5a17f
thly112 <thaolythly@gmail.com>
2025-03-21 07:37:43 +0700

    first commit

---
+++ app/src/main/java/com/example/project_gk/IntroActivity.java
@@ -0,0 +1,14 @@
+package com.example.project_gk;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class IntroActivity extends AppCompatActivity {
+
+    }
